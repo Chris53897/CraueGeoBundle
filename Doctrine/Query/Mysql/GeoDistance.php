32676eb439bbe323ab341ac4bcf830e629dcc157
Christopher Georg <christopher.georg@sr-travel.de>
2024-02-24 11:18:31 +0100

    feat: bump min version symfony 6.3 => 6.4 migrate doctrine/orm 2 => 3 migrate doctrine/dbal 3 => 4

--- Doctrine/Query/Mysql/GeoDistance.php
+++ Doctrine/Query/Mysql/GeoDistance.php
@@ -7,7 +7,8 @@ use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Parser;
 use Doctrine\ORM\Query\QueryException;
 use Doctrine\ORM\Query\SqlWalker;
-use Doctrine\ORM\Query\TokenType;
+use Doctrine\ORM\Query\Lexer;
+#use Doctrine\ORM\Query\TokenType;
 
 /**
  * Usage: GEO_DISTANCE(latOrigin, lngOrigin, latDestination, lngDestination)
@@ -30,16 +31,16 @@ class GeoDistance extends FunctionNode {
      * @throws QueryException
      */
     public function parse(Parser $parser) : void {
-		$parser->match(TokenType::T_IDENTIFIER);
-		$parser->match(TokenType::T_OPEN_PARENTHESIS);
+		$parser->match(Lexer::T_IDENTIFIER);
+		$parser->match(Lexer::T_OPEN_PARENTHESIS);
 		$this->latOrigin = $parser->ArithmeticExpression();
-		$parser->match(TokenType::T_COMMA);
+		$parser->match(Lexer::T_COMMA);
 		$this->lngOrigin = $parser->ArithmeticExpression();
-		$parser->match(TokenType::T_COMMA);
+		$parser->match(Lexer::T_COMMA);
 		$this->latDestination = $parser->ArithmeticExpression();
-		$parser->match(TokenType::T_COMMA);
+		$parser->match(Lexer::T_COMMA);
 		$this->lngDestination = $parser->ArithmeticExpression();
-		$parser->match(TokenType::T_CLOSE_PARENTHESIS);
+		$parser->match(Lexer::T_CLOSE_PARENTHESIS);
 	}
 
 	public function getSql(SqlWalker $sqlWalker) : string {
